9fca6d1f379de17548114d8e635814ceaf2661c0
subhranshu sekher dhal <ssdhal98@gmail.com>
2024-03-27 12:05:45 +0530

    added post & put req

--- index.js
+++ index.js
@@ -8,7 +8,13 @@ app.get("/",function(req,res){
     res.send("Samrat !")
 })
 
+app.post("/",function(req,res){
+    res.send("got a post request")
+})
 
+app.put("/user",function(req,res){
+    res.send("got a put request at /user")
+})
 
 app.listen(port,()=>{
 console.log(`app listening in port ${port}`)
