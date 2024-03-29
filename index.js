0d8e96bfa93ef2f899550222d660e2ff0998cb70
subhranshu sekher dhal <ssdhal98@gmail.com>
2024-03-29 15:37:18 +0530

    adding body parser

--- index.js
+++ index.js
@@ -4,21 +4,43 @@ const app = express();
 
 const port = 3000;
 
+// const myLogger = function(req,res,next){
+//     console.log('logged!')
+//     next()
+// }
+
+// app.use(myLogger)
+
 app.get("/",function(req,res){
     res.send("Samrat !")
 })
 
-app.post("/",function(req,res){
-    res.send("got a post request")
+app.get("/routeHandler", function(req,res){
+    // res.json({
+    //     name:"samrat",
+    //     age:"25"
+    // })
+    res.send('<h1>hello</h1>')
 })
 
-app.put("/user",function(req,res){
-    res.send("got a put request at /user")
+app.post("/conversation",function(req,res){
+    console.log(req.body)
+    res.send({
+        msg: "2 + 2 = 4"
+    })
 })
 
-app.delete("/user",function(req,res){
-    res.send("got a delete request at /user")
-})
+// app.post("/",function(req,res){
+//     res.send("got a post request")
+// })
+
+// app.put("/user",function(req,res){
+//     res.send("got a put request at /user")
+// })
+
+// app.delete("/user",function(req,res){
+//     res.send("got a delete request at /user")
+// })
 
 app.listen(port,()=>{
 console.log(`app listening in port ${port}`)
