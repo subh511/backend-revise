b8dc70394c0aa5fe872c90f08c01c451fabb2888
subhranshu sekher dhal <ssdhal98@gmail.com>
2024-03-30 12:18:33 +0530

    creating hospital scenario

---
+++ hospital.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const app = express();
+
+/**
+ * GET Method: is asking something to the server;
+ * POST Method: create something on internet;
+ * PUT Method: put is used to edit some data/or put something in backend;
+ * DELETE Method: delete is used to delete something from backend
+ */
+
+app.listen(3000)
